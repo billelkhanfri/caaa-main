9874ed8989554738194dc56b7261a3a420df293e
billelkhanfri <khanfri.billel@yahoo.fr>
2025-12-15 12:39:39 +0100

    add sanity files

---
+++ caaa-studio/sanity/lib/client.js
@@ -0,0 +1,9 @@
+// sanity/client.js
+import { createClient } from "next-sanity";
+
+export const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: "2025-12-14", 
+  useCdn: false, 
+});
